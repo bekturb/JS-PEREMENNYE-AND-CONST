2d7abd6aecdf141a82d9f93ae04a3963639458db
bekturb <bekkgboy2@gmail.com>
2022-03-01 05:33:35 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+// let userName;
+// let userAddress;
+
+// let num1 = 200;
+// let num2 = 2021;
+// document.write('<h1>Number1:</h1>' + " " + num1);
+// document.write('<h1>Number2:</h1>' + " " + num2);
+
+
+// let userName = prompt('What is your name?');
+// let userAge = prompt('How old are you?');
+// let userLocation = prompt('What is your address?');
+//
+// document.write('<h3>Твои данные:</h3>' + );
+// document.write('<h3>Твой возраст:</h3>' + userAge);
+// document.write('<h3>Твой город:</h3>' + userLocation);
+
+                //Hometask
+
+// let name = 'iVan';
+//
+// console.log(name);
+//
+//  name = 'Ivan';
+//
+// console.log(name);
+
+// let money = "$120";
+//
+// money = "120";
+//
+// console.log(money);
+
+// let computer1 = prompt("Здравствуй, я компьютер, а тебя как зовут?");
+// let computer2 = prompt("Очень приятно "  + " " + computer1 + '.' + ' ' + "Сколько тебе лет?");
+// let computer3 = prompt("Ого! Тебе уже" + ' ' +  computer2 + "!");
+
+
